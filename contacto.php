418df97b1ba335b3c0d143b601fd2d5686cab26b
matequin <matequin10@hotmail.com>
2021-10-06 12:43:23 -0500

    segundoCommit

---
+++ contacto.php
@@ -0,0 +1,11 @@
+.page-chrome_newtab {
+    outline: none;
+    overflow-y: auto;
+}
+.body--home, .site-wrapper--home {
+    background: #f7f7f7;
+}
+body, input, select, textarea {
+    font-family: "DDG_ProximaNova","DDG_ProximaNova_UI_0","DDG_ProximaNova_UI_1","DDG_ProximaNova_UI_2","DDG_ProximaNova_UI_3","DDG_ProximaNova_UI_4","DDG_ProximaNova_UI_5","DDG_ProximaNova_UI_6","Proxima Nova","Helvetica Neue","Helvetica","Segoe UI","Nimbus Sans L","Liberation Sans","Open Sans",FreeSans,Arial,sans-serif;
+    color: #222;
+}
